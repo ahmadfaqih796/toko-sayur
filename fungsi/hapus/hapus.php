e006a32b4bbb4f70bee244ee49a9868aeff8d25b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-10-29 13:02:58 +0700

    delete satuan function

--- fungsi/hapus/hapus.php
+++ fungsi/hapus/hapus.php
@@ -4,29 +4,38 @@ session_start();
 if (!empty($_SESSION['admin'])) {
     require '../../config.php';
     if (!empty(htmlentities($_GET['kategori']))) {
-        $id= htmlentities($_GET['id']);
+        $id = htmlentities($_GET['id']);
         $data[] = $id;
         $sql = 'DELETE FROM kategori WHERE id_kategori=?';
-        $row = $config -> prepare($sql);
-        $row -> execute($data);
+        $row = $config->prepare($sql);
+        $row->execute($data);
         echo '<script>window.location="../../index.php?page=kategori&&remove=hapus-data"</script>';
     }
 
+    if (!empty(htmlentities($_GET['satuan']))) {
+        $id = htmlentities($_GET['id']);
+        $data[] = $id;
+        $sql = 'DELETE FROM satuan WHERE id_satuan=?';
+        $row = $config->prepare($sql);
+        $row->execute($data);
+        echo '<script>window.location="../../index.php?page=satuan&&remove=hapus-data"</script>';
+    }
+
     if (!empty(htmlentities($_GET['barang']))) {
-        $id= htmlentities($_GET['id']);
+        $id = htmlentities($_GET['id']);
         $data[] = $id;
         $sql = 'DELETE FROM barang WHERE id_barang=?';
-        $row = $config -> prepare($sql);
-        $row -> execute($data);
+        $row = $config->prepare($sql);
+        $row->execute($data);
         echo '<script>window.location="../../index.php?page=barang&&remove=hapus-data"</script>';
     }
 
     if (!empty(htmlentities($_GET['jual']))) {
         $dataI[] = htmlentities($_GET['brg']);
         $sqlI = 'select*from barang where id_barang=?';
-        $rowI = $config -> prepare($sqlI);
-        $rowI -> execute($dataI);
-        $hasil = $rowI -> fetch();
+        $rowI = $config->prepare($sqlI);
+        $rowI->execute($dataI);
+        $hasil = $rowI->fetch();
 
         /*$jml = htmlentities($_GET['jml']) + $hasil['stok'];
 
@@ -39,22 +48,22 @@ if (!empty($_SESSION['admin'])) {
         $id = htmlentities($_GET['id']);
         $data[] = $id;
         $sql = 'DELETE FROM penjualan WHERE id_penjualan=?';
-        $row = $config -> prepare($sql);
-        $row -> execute($data);
+        $row = $config->prepare($sql);
+        $row->execute($data);
         echo '<script>window.location="../../index.php?page=jual"</script>';
     }
 
     if (!empty(htmlentities($_GET['penjualan']))) {
         $sql = 'DELETE FROM penjualan';
-        $row = $config -> prepare($sql);
-        $row -> execute();
+        $row = $config->prepare($sql);
+        $row->execute();
         echo '<script>window.location="../../index.php?page=jual"</script>';
     }
-    
+
     if (!empty(htmlentities($_GET['laporan']))) {
         $sql = 'DELETE FROM nota';
-        $row = $config -> prepare($sql);
-        $row -> execute();
+        $row = $config->prepare($sql);
+        $row->execute();
         echo '<script>window.location="../../index.php?page=laporan&remove=hapus"</script>';
     }
 }
